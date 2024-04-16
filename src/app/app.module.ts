2fa7f67e04c437c62f95ac8553e42b87006b72c9
Joice Fogaça <joi.fogaca@gmail.com>
2024-04-15 21:07:41 -0300

    Correção imports material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import {  MatButtonModule } from '@angular/material/button';
 import { SearchMusicComponent } from './search-music/search-music.component';
 import { FormsModule } from '@angular/forms';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { TrackItemComponent } from './track-item/track-item.component';
 import { MatIconModule } from '@angular/material/icon';
